a79119eb1ae3e694882c5ed7c617bc41594abe67
Sasa Nedic <svetasmak0@gmail.com>
2024-04-17 19:33:50 +0200

    Create Home.tsx component

---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+
+const Home = () => {
+    return(
+        <div className="container">
+            <div className="text">
+                <h1>We are changing the way people shop</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore repellat explicabo enim soluta temporibus asperiores aut obcaecati perferendis porro nobis.</p>
+                <button>OUR PRODUCTS</button>
+            </div>
+            <div className="image">
+                <img src="/public/comfy-main-photojpg.webp" alt="Product" />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
