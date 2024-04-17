ecdeef46bdf4d4ba2ba0efd831436c767ddcc881
Sasa Nedic <svetasmak0@gmail.com>
2024-04-17 20:12:36 +0200

    Create NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <div>
+            <ul>
+                <li>COMFY</li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Price</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    )
+}
+export default NavBar;
