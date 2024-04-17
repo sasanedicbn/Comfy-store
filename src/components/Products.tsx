1c35cffa29ee863d732ab56b4e8adc07a97f59af
Sasa Nedic <svetasmak0@gmail.com>
2024-04-18 00:03:16 +0200

    Create ux style

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,16 +6,16 @@ const Products = () => {
     >
       <div className="form-control">
         <label htmlFor="search" className="label">
-          <span className="label-text capitalize">search product</span>
+          <span className="label-text capitalize">Search product</span>
         </label>
         <input type="search" name="search" className="input input-bordered input-sm" value="" />
       </div>
       <div className="form-control">
         <label htmlFor="category" className="label">
-          <span className="label-text capitalize">select category</span>
+          <span className="label-text capitalize">Select category</span>
         </label>
         <select name="category" id="category" className="select select-bordered select-sm">
-          <option value="all" selected="">all</option>
+          <option value="all" >all</option>
           <option value="Tables">Tables</option>
           <option value="Chairs">Chairs</option>
           <option value="Kids">Kids</option>
@@ -28,7 +28,7 @@ const Products = () => {
           <span className="label-text capitalize">select company</span>
         </label>
         <select name="company" id="company" className="select select-bordered select-sm">
-          <option value="all" selected="">all</option>
+          <option value="all" >all</option>
           <option value="Modenza">Modenza</option>
           <option value="Luxora">Luxora</option>
           <option value="Artifex">Artifex</option>
@@ -41,7 +41,7 @@ const Products = () => {
           <span className="label-text capitalize">sort by</span>
         </label>
         <select name="order" id="order" className="select select-bordered select-sm">
-          <option value="a-z" selected="">a-z</option>
+          <option value="a-z" >a-z</option>
           <option value="z-a">z-a</option>
           <option value="high">high</option>
           <option value="low">low</option>
@@ -49,23 +49,23 @@ const Products = () => {
       </div>
       <div className="form-control">
         <label htmlFor="price" className="label cursor-pointer">
-          <span className="label-text capitalize">select price</span>
-          <span>$1,000.00</span>
+          <span className="label-text capitalize">Select price</span>
+          <span className="current-price">$1,000.00</span>
         </label>
-        <input type="range" name="price" min="0" max="100000" className="range range-primary range-sm" step="1000" value="100000" />
-        <div className="w-full flex justify-between text-xs px-2 mt-2">
-          <span className="font-bold text-md">0</span>
-          <span className="font-bold text-md">Max : $1,000.00</span>
+        <input type="range" name="price" min="0" max="100000" className="range range-primary " step="1000" value="100000" />
+        <div className="">
+          <span className="min-price">0</span>
+          <span className="max-price">Max : $1,000.00</span>
         </div>
       </div>
       <div className="form-control items-center">
         <label htmlFor="shipping" className="label cursor-pointer">
-          <span className="label-text capitalize">free shipping</span>
+          <span className="free-shipping">Free shipping</span>
         </label>
         <input type="checkbox" name="shipping" className="checkbox checkbox-primary checkbox-sm" />
       </div>
-      <button type="submit" className="btn btn-primary btn-sm">search</button>
-      <a href="/products" className="btn btn-accent btn-sm">reset</a>
+      <button type="submit" className="btn btn-primary ">Search</button>
+      <button type="submit" className="btn btn-primary  green">Reset</button>
     </form>
     )
 }
