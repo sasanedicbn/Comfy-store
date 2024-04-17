23c175d75ebf8d84f6db22a15a77d15b4ff71e54
Sasa Nedic <svetasmak0@gmail.com>
2024-04-17 19:41:10 +0200

    Create About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return(
+        <div className="about-container">
+            <h1 className="comfy-title">We love comfy</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quae quam blanditiis vitae, dolor non eveniet ipsum voluptatibus, quia optio aut! Perferendis ipsa cumque ipsam nostrum reprehenderit ad illo sed officiis ea tempore! Similique eos minima sit porro, ratione aspernatur!</p>
+        </div>
+    );
+}
+
+export default About;
