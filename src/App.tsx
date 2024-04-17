e887847eef032f8eaf651462b6172e378eebd876
Sasa Nedic <svetasmak0@gmail.com>
2024-04-17 21:02:47 +0200

    Add react router in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 
 import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import About from './components/About'
 import Home from './components/Home'
+import Products from './components/Products'
 import NavBar from './components/NavBar'
 
+const router = createBrowserRouter([
+  {path: '/', element: <NavBar/>},
+  {path: '/products', element: <Products/> }
+])
 function App() {
 
 
   return (
-    <>
-    <NavBar/>
-     {/* <Home/> */}
-     <About/>
-    </>
+   <RouterProvider router={router}/>
   )
 }
 
